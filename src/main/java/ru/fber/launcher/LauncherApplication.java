3049e49864d3fe1c5c888cc40baa5172e9104047
tercoz <tercoz@mail.ru>
2021-04-08 12:15:44 +0300

    Launcher is polished a little bit

--- src/main/java/ru/fber/launcher/LauncherApplication.java
+++ src/main/java/ru/fber/launcher/LauncherApplication.java
@@ -2,28 +2,20 @@ package ru.fber.launcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Collections;
-import java.util.Map;
-
-import static java.lang.String.format;
+import javax.servlet.http.HttpSession;
 
 @SpringBootApplication
 @RestController
 public class LauncherApplication {
 
-    @GetMapping("/redirect")
-    public ModelAndView lk() {
-        return new ModelAndView("redirect:http://localhost:9080/lk");
+    @RequestMapping("/")
+    public ModelAndView root(HttpSession session) {
+        session.setAttribute("slavs", "here");
+        return new ModelAndView("redirect:http://localhost:9080");
     }
 
     public static void main(String[] args) {
